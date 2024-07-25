7d52c38101f2dd21aa2ae0ad49fbf59a44a46111
fpunny <frederic.pun@gmail.com>
2024-07-25 16:10:36 -0400

    uwu

--- src/middleware.ts
+++ src/middleware.ts
@@ -57,7 +57,7 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    '/discord/*',
+    '/discord/.*',
     '/resources',
     '/schedule',
     '/closed',
