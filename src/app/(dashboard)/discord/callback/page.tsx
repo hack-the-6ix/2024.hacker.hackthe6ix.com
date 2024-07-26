582027860d5d7533e250ff3db7696b8b354b6f55
jeffrey-zang <jeffrey.zang.07@gmail.com>
2024-07-25 21:02:55 -0400

    error messages on discord callback

--- src/app/(dashboard)/discord/callback/page.tsx
+++ src/app/(dashboard)/discord/callback/page.tsx
@@ -1,14 +1,36 @@
 import { redirect } from 'next/navigation';
 import { fetchHt6 } from '@/api';
 import type Ht6Api from '@/api.d';
+import { Page } from '@/app/(dashboard)/rsvp/shared';
+import Text from '@/components/Text';
 
 async function DiscordCallbackPage({
   searchParams,
 }: {
   searchParams: { state?: string; code?: string };
 }) {
-  if (!searchParams.state || !searchParams.code) {
-    return <p>ur bad lmao</p>;
+  const { message: profile } = await fetchHt6<
+    Ht6Api.ApiResponse<Ht6Api.HackerProfile>
+  >('/api/action/profile');
+
+  if (
+    !searchParams.state ||
+    !searchParams.code ||
+    Array.isArray(searchParams.state) ||
+    Array.isArray(searchParams.code)
+  ) {
+    return (
+      <Page
+        name={profile.firstName}
+        title={<>Something went wrong...</>}
+        welcome
+      >
+        <Text textAlign="center" as="p">
+          An error occured while trying to associate your Discord account.
+          Please try again.
+        </Text>
+      </Page>
+    );
   }
 
   const { status } = await fetchHt6<
@@ -22,7 +44,18 @@ async function DiscordCallbackPage({
     },
   });
 
-  return redirect('https://discord.gg/ZzYZZKyxSs');
+  if (status === 200) {
+    redirect('https://discord.gg/ZzYZZKyxSs');
+  }
+
+  return (
+    <Page name={profile.firstName} title={<>Something went wrong...</>} welcome>
+      <Text textAlign="center" as="p">
+        An error occured while trying to associate your Discord account. Please
+        try again.
+      </Text>
+    </Page>
+  );
 }
 
 export default DiscordCallbackPage;
